3af2b97982e1acdd0eb1d7aaf9357da37254c2c6
Alex-17531412 <alexanderadam636@gmail.com>
2023-08-04 16:03:35 +0700

    Add support avi and mp4

--- main.py
+++ main.py
@@ -3,25 +3,47 @@ import os
 from tqdm import tqdm
 
 
-image_folder = 'Data/Frames_CHK'
-video_name = f'Output/Result.mp4'
-fps = 30.0
+def render_Video(image_folder, videoName, fps: float, formatVid="mp4"):
+    folder = f"Input/{image_folder}"
+    video_name = f'Output/{videoName}.{formatVid}'
+    
+    Frames = []
 
-Frames = []
+    for files_ in os.listdir(folder):
+        if os.path.isfile(os.path.join(folder, files_)):
+            Frames.append(files_)
+    sorted(Frames)
 
-for files_ in os.listdir(image_folder):
-    if os.path.isfile(os.path.join(image_folder,files_)):
-        Frames.append(files_)
+    image = f'{folder}/{Frames[0]}'
+    height, width, channel = cv2.imread(image).shape
 
-image = f"{image_folder}/{Frames[0]}"
-height, width, channel = cv2.imread(image).shape
+    fourcc = None
 
-fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
-video = cv2.VideoWriter(video_name, fourcc, fps,(width, height))
+    if formatVid == "avi":
+        fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    elif formatVid == "mp4":
+        fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
+    else:
+        print("Format UnSupported")
+        cv2.destroyAllWindows()
 
-for frame in tqdm(Frames, colour='#35d70d'):
-    data = cv2.imread((f"{image_folder}/{frame}"))
-    video.write(data)
+    video = cv2.VideoWriter(video_name, fourcc, fps, (width,height))
 
-cv2.destroyAllWindows()
-video.release()
+    for frame in tqdm(Frames, colour='#35d70d'):
+        data = cv2.imread((f"{folder}/{frame}"))
+        video.write(data)
+
+
+    
+if __name__ == "__main__":
+    Frame_folder = input("Folder Name: ")
+    Video_name = input("Name of Video: ")
+    fps = float(input("Enter fps: "))
+    format_vid = input("Format avi/mp4: ")
+
+    render_Video(Frame_folder, Video_name, fps, format_vid)
+
+    cv2.destroyAllWindows()
+    cv2.VideoWriter().release()
+
+    
